cd53bbe92e11d8498bee4cd081574bb7fe054501
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-03-31 14:38:40 -0300

    Testing conditionals

---
+++ initials/conditionals.py
@@ -0,0 +1,12 @@
+my_boolean = True
+
+if my_boolean:
+    print('The boolean is true')
+else:
+    print('The boolean is false')
+
+print(my_boolean and True)
+print(my_boolean or False)
+print(not my_boolean)
+print(3 > 3.1)
+print(True == 1)
