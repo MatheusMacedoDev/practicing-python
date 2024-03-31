cae5c7b33c52082a9da527ff4c68e60064670f01
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-03-31 14:28:03 -0300

    Creating a hello word testing strings

---
+++ initials/hello-world.py
@@ -0,0 +1,18 @@
+import string
+
+print('Hello, world!')
+
+mystring = 'matheus macedo'
+mystring = mystring.title()
+
+print(mystring)
+print(len(mystring))
+print(mystring.split(' '))
+print(mystring[4])
+print(mystring.casefold())
+print(mystring.center(100))
+print(f'My string is {mystring=}')
+print(string.ascii_letters)
+print(string.hexdigits)
+print(string.punctuation)
+print('4'.isdigit())
