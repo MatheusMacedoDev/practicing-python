f678bbf9be5990ce0fa486a6a0475c0c39210376
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-03-31 17:17:02 -0300

    Working with tuples

---
+++ initials/tuples.py
@@ -0,0 +1,11 @@
+my_tuple = (1, 2, 3)
+my_tuple = tuple([1, 3, 2])
+my_tuple = 1, 2, 3
+
+my_second_tuple = (*my_tuple, 4, 5, 6)
+
+print(my_tuple)
+print(my_second_tuple)
+
+print(list(my_tuple))
+print(set([1, 2, 3]))
