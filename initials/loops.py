7c01f7f812f2d6cedf58d864e5d96a598e0bdc0a
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-03-31 14:55:30 -0300

    Testing loops

---
+++ initials/loops.py
@@ -0,0 +1,24 @@
+for letter in 'Matheus':
+    print(letter)
+
+fruits = [
+    'Melon',
+    'Watermelon',
+    'Orange',
+    'Banana',
+    'Apple'
+]
+
+print()
+
+for fruit in fruits:
+    print(fruit)
+
+print(fruits[0])
+
+print()
+
+i = 1
+while i <= 4:
+    print(i)
+    i += 1
